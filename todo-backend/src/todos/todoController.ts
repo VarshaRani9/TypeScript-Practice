5ad062384391466af80a6568f013f973abc69dd6
Varsha <varsharranijnv16122001@gmail.com>
2024-08-17 09:03:51 +0530

    new commit

---
+++ todo-backend/src/todos/todoController.ts
@@ -0,0 +1,26 @@
+import {
+    Body,
+    Controller,
+    Get,
+    Path,
+    Post,
+    Query,
+    Route,
+    SuccessResponse,
+  } from "tsoa";
+  import { Todo } from "./todo";
+  import { todoService, TodoCreationParams } from "./todoService";
+  
+  @Route("todo")
+  export class todoController extends Controller {
+
+    @Get("{id}")
+    public async getTodo(
+      @Path() id: string
+    ): Promise<Todo>{
+        let todoServ =  new todoService()
+        return todoServ.get(id);
+
+    }
+    
+}
